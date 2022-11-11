5fff70aca0f892e711f00b3b53d1814b9ba7fdf0
umisora <bit72s_akm@hotmail.com>
2022-11-11 16:01:35 +0900

    Update sample

--- examples/itmc_integration.tf
+++ examples/itmc_integration.tf
@@ -5,3 +5,6 @@ module "itmc-integration" {
 output "itmc_role_arn" {
   value = module.itmc-integration.role_arn
 }
+output "itmc_role_external_id" {
+  value = module.itmc-integration.external_id
+}
